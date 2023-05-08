6526dba0dd8c65636419a03a3d3c39de061d5a26
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2023-05-08 16:45:44 -0300

    ✅ Teste de Rota

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './paginas/Principal/App';
+import Cartoes from './componentes/Cartoes';
 
 describe('Rotas', () => {
   test('Deve renderizar a rota principal', () => {
@@ -9,5 +10,20 @@ describe('Rotas', () => {
     const usuario = screen.getByText('Olá, Joana :)!');
 
     expect(usuario).toBeInTheDocument();
+  });
+
+  test('Deve renderizar a rota Cartões', () => {
+    const rota = '/cartoes';
+    render(
+      <MemoryRouter initialEntries={[rota]}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="cartoes" element={<Cartoes />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const meusCartoes = screen.getByText('Meus cartões');
+    expect(meusCartoes).toHaveTextContent('Meus cartões');
   });
 });
